f5687c832cdd29a094a5d2e28de9f90bb22c2973
Spyx34 <82611444+Spyx34@users.noreply.github.com>
2021-08-18 21:51:45 +0200

    Update main.py
    pm and rick roll jetzt möglich

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import random as r
 import discord
 from discord.ext import commands
 import asyncio
-from discord.utils import get
-from discord.ext.commands import Bot
-from discord import User
-import time
 import giphy_client
 from giphy_client.rest import ApiException
+from discord.utils import get
+import time
+
+intents = discord.Intents.default()
+intents.members = True
 
 TOKEN = 'ODc3MzEwNTgxMDE3ODIxMjI1.YRwxJg.YguoUXUYdmBbj3TWWzDjH9Zyvlg'
 
-client = commands.Bot(command_prefix = "#")
+client = commands.Bot(command_prefix = "#", intents = intents)
 
 
 
@@ -20,6 +21,13 @@ client = commands.Bot(command_prefix = "#")
 @client.event
 async def on_ready():
     print("Logges in as "+ client.user.name+"\n")
+
+@client.event
+async def on_member_join(member):
+    server_name = client.get_guild(802985801100165200)
+    await member.send(f'Willkommen auf {server_name}!')
+    print(1)
+
 
 @client.command()
 async def info(ctx, *args):
@@ -32,6 +40,12 @@ async def info(ctx, *args):
             await ctx.send("Use #truck <user> to warn your friend of an incoming truck")
         elif args[0] == "ssp":
             await ctx.send("Use #ssp <schere/stein/papier> to play Rock Paper Scissors")
+        elif args[0] == "clear" or args[0] == "do_woman":
+            await ctx.send("Use #clear <amount> to clear a certain amount of messages")
+        elif args[0] == "gif":
+            await ctx.send("Use #gif <gif_name> to search and send a gif \nAliases: searchgif")
+        elif args[0] == "gif":
+            await ctx.send("Use #rickroll <user> to rickroll someone \nAliases: rick , rickastley")
     except:
         await ctx.send('''.
                           -------HELP-MENU-------
@@ -39,6 +53,9 @@ async def info(ctx, *args):
                           #do_bezos to get bezost
                           #truck summon a truck
                           #ssp play rock paper scissors
+                          #gif for a gif
+                          #clear to clear old messages
+                          #rickroll to rickroll some1
                           -------------------------------
                           Use #info <command> for more information''')
 
@@ -49,12 +66,10 @@ async def on_message_delete(message):
     Autor = message.author
     x = Inhalt + "\n--" + str(Autor)
     await channel.send(x)
-    
-    
-@client.command()
-async def clear(ctx, Anzahl = 15):
-    await ctx.channel.purge(limit=int(Anzahl))
 
+@client.command(aliases=['do_woman'])
+async def clear(ctx, Anzahl = 15):
+    await ctx.channel.purge(limit=int(Anzahl) + 1)
 
 
 @client.command()
@@ -93,8 +108,6 @@ async def ssp(ctx,*args):
     elif args[0]:
         print("that Motherfucker used hund")
 
-
-
 @client.command()
 async def truck(ctx, user: discord.Member):
     channel = ctx.channel
@@ -116,8 +129,20 @@ async def truck(ctx, user: discord.Member):
         else:
             await channel.send(f"{user.mention} springt über den Truck und überlebt!")
 
-            
-@client.command(aliases=['gif', 'searchgif'])
+@client.command(aliases=[ 'searchgif'])
+async def gif(ctx,*,Suche="dog"):
+    api_key = 'GSGMjDZvTnm9Ks5ZBBmKnk2hXTOOVHPq'
+    api_instance = giphy_client.DefaultApi()
+
+    try:
+        api_responce = api_instance.gifs_search_get(api_key, Suche, limit = 5) #limit gibt limit für return der anzahl an gifs an
+        gif_liste = list(api_responce.data)
+        gif = r.choice(gif_liste)
+        await ctx.channel.send(gif.embed_url)
+    except ApiException as e:
+        print('fehlversuch gif aufzurufen mit api--> check api key')
+
+@client.command(aliases=[ 'goodnight'])
 async def gutenacht(ctx,*,Suche="good night"):
     api_key = 'GSGMjDZvTnm9Ks5ZBBmKnk2hXTOOVHPq'
     api_instance = giphy_client.DefaultApi()
@@ -130,29 +155,23 @@ async def gutenacht(ctx,*,Suche="good night"):
     except ApiException as e:
         print('fehlversuch gif aufzurufen mit api--> check api key')
 
+@client.command(aliases=[ 'rickastley', 'rick'])
+async def rickroll(ctx,*,user : discord.Member,Suche="rickroll"):
+    api_key = 'GSGMjDZvTnm9Ks5ZBBmKnk2hXTOOVHPq'
+    api_instance = giphy_client.DefaultApi()
 
-#@client.command()
-#async def kill(ctx, user: discord.Member):
-#    await ctx.send(f'{user.mention} stirbt')
-#
-#    global todesliste
-#    todesliste = {}
-#    try:
-#        tempo = todesliste[f'{user}']
-#        todesliste[f'{user}'] = tempo + 1
-#    except:
-#        temporaer = {f'{user}' : 1}
-#        todesliste = todesliste | temporaer
-#
-#@client.command()
-#async def rip(ctx, user: discord.Member):
-#    #print(todesliste[f'{user}'])
-#    await ctx.send(f'{user} starb ' + str(todesliste[f'{user}']) + 'mal')
+    try:
+        api_responce = api_instance.gifs_search_get(api_key, Suche, limit = 1) #limit gibt limit für return der anzahl an gifs an
+        gif_liste = list(api_responce.data)
+        gif = r.choice(gif_liste)
+        await ctx.channel.send(gif.embed_url)
+    except ApiException as e:
+        print('fehlversuch gif aufzurufen mit api--> check api key')
+    await ctx.send(f'{user.mention} ,du wurdest gerade von {ctx.author} gerickrolled!')
 
-
-
-
-
+@client.command()
+async def kill(ctx, user: discord.Member):
+    await ctx.send(f'{user.mention} stirbt')
 
 
 client.run(TOKEN)
